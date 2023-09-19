1f772c8182333ece00a283120557838006fe4b4f
Dawid Pawłowski <d.pawlowski@cksource.com>
2023-09-19 15:56:59 +0200

    add application bootstrap

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+/*
+ Copyright (c), CKSource Holding sp. z o.o. All rights reserved.
+ */
+require( '@cksource-cs/eslint-config-cs-module/eslint-patch-dependency-loader' );
+
+module.exports = {
+	'root': true,
+	'extends': '@cksource-cs/eslint-config-cs-module/typescript'
+};
---
+++ src/server.ts
@@ -0,0 +1,6 @@
+/*
+ Copyright (c), CKSource Holding sp. z o.o. All rights reserved.
+ */
+
+// @ts-ignore test hello word
+console.log( 'Hello word!' );
