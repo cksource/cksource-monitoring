fb765c5f5d5661d82030fce9bd2d4936df32e245
Dawid Pawłowski <d.pawlowski@cksource.com>
2023-09-21 11:49:03 +0200

    infra cleanup

---
+++ infrastructure/scripts/setup-infra.js
@@ -0,0 +1,9 @@
+/*
+ Copyright (c), CKSource Holding sp. z o.o. All rights reserved.
+ */
+
+const setupGrafana = require( './setup-grafana' );
+
+( async function() {
+	await setupGrafana();
+}() );
