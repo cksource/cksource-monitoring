ea815a62909d5916b60c8a8f390385ace1b0084d
Dawid Pawłowski <d.pawlowski@cksource.com>
2023-09-25 09:46:54 +0200

    improve sensitive of fails counter

--- src/TestsRunner.ts
+++ src/TestsRunner.ts
@@ -2,16 +2,23 @@
  Copyright (c), CKSource Holding sp. z o.o. All rights reserved.
  */
 
+import { Counter } from 'prom-client';
+
 import { ITest } from './tests/Test';
 import { IMetrics, StopTimerFunction } from './Metrics';
 
 const METRIC_NAME: string = 'monitoring_test';
+const GAUGE_NAME: string = 'monitoring_test_fails';
 
 export default class TestsRunner {
+	private readonly _counter: Counter;
+
 	public constructor(
 		private readonly _metrics: IMetrics,
 		private readonly _tests: ITest[]
-	) {}
+	) {
+		this._counter = this._metrics.counter( GAUGE_NAME, [ 'test_name', 'product_name' ] );
+	}
 
 	public async runTests(): Promise<void> {
 		await Promise.all( this._tests.map( test => this._runTest( test ) ) );
@@ -25,6 +32,9 @@ export default class TestsRunner {
 		try {
 			await test.run();
 		} catch ( error ) {
+			// eslint-disable-next-line no-console
+			console.log( `Test - ${ test.productName } - failed.` );
+
 			statusCode = error.statusCode ?? 500;
 		} finally {
 			stopTimer( {
@@ -32,6 +42,12 @@ export default class TestsRunner {
 				test_name: test.testName,
 				product_name: test.productName
 			} );
+
+			// Using separate counter for failed tests is more precise if we want to detect rare and single fails.
+			this._counter.labels( {
+				test_name: test.testName,
+				product_name: test.productName
+			} ).inc( statusCode > 399 ? 1 : 0 );
 		}
 	}
 
