7b95e30aefa1f290c333843966a9aee08ceacbd9
Dawid Pawłowski <d.pawlowski@cksource.com>
2023-10-31 16:54:11 +0100

    import first alert

--- infrastructure/scripts/setup-grafana.js
+++ infrastructure/scripts/setup-grafana.js
@@ -6,8 +6,10 @@
 
 const { HttpClient } = require( '@cksource-cs/http-client-module' );
 
-const prometheusDataSource = require( '../grafana/prometheus-data-source.json' );
-const cksourceMonitoringDashboard = require( '../grafana/dashboards/cksource-monitoring.json' );
+const prometheusDataSource = require( '../grafana/datasources/prometheus-data-source2.json' );
+const folder = require( '../grafana/folders/cksource-monitoring-folder.json' );
+const cksourceMonitoringDashboard = require( '../grafana/dashboards/cksource-monitoring-dashboard2.json' );
+const alerts = require( '../grafana/alerts/test-alerts2.json' );
 
 const GRAFANA_URL = 'http://localhost:3000';
 const GRAFANA_AUTH = 'cks:pass';
@@ -22,43 +24,38 @@ async function setupGrafana() {
 
 		console.log( 'Grafana is ready.' );
 
-		let response = await httpClient.post( `${ GRAFANA_URL }/api/datasources`, {
-			auth: GRAFANA_AUTH,
-			body: JSON.stringify( prometheusDataSource ),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		} );
+		await _importDataSource();
 
-		if ( response.statusCode !== 200 ) {
-			throw new Error( response.text() );
-		}
+		await _importFolder();
 
-		console.log( 'Grafana data source imported.' );
+		await _importDashboards();
 
-		response = await httpClient.post( `${ GRAFANA_URL }/api/dashboards/db`, {
-			auth: GRAFANA_AUTH,
-			body: JSON.stringify( {
-				dashboard: {
-					...cksourceMonitoringDashboard,
-					id: null,
-					uid: null
-				},
-				folderId: 0
-			} ),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		} );
-
-		if ( response.statusCode !== 200 ) {
-			throw new Error( response.text() );
-		}
-
-		console.log( 'Grafana dashboards imported.' );
+		await _importAlerts();
 	} catch ( error ) {
 		console.log( error );
 	}
+}
+
+function _importDataSource() {
+	return _import( 'datasources', prometheusDataSource );
+}
+
+function _importDashboards() {
+	return _import( 'dashboards/db', {
+		dashboard: {
+			...cksourceMonitoringDashboard.dashboard,
+			id: null
+		},
+		folderUid: folder.uid
+	} );
+}
+
+function _importAlerts() {
+	return _import( 'v1/provisioning/alert-rules', { ...alerts[ 0 ], folderUID: folder.uid } );
+}
+
+function _importFolder() {
+	return _import( 'folders', folder );
 }
 
 async function _waitForGrafanaReadiness() {
@@ -83,6 +80,22 @@ function _wait( time ) {
 	return new Promise( resolve => {
 		setTimeout( resolve, time );
 	} );
+}
+
+async function _import( apiPath, body ) {
+	const response = await httpClient.post( `${ GRAFANA_URL }/api/${ apiPath }`, {
+		auth: GRAFANA_AUTH,
+		body: JSON.stringify( body ),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	} );
+
+	if ( response.statusCode > 299 ) {
+		throw new Error( response.text() );
+	}
+
+	console.log( `Grafana ${ apiPath } imported.` );
 }
 
 module.exports = setupGrafana;
