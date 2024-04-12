d5707980da8b0d02fb9081abb7b1d2333ab236e8
sdomas-cksource <93912949+sdomas-cksource@users.noreply.github.com>
2024-04-12 10:11:33 +0200

    ci: Use oidc role for interacting with storybook infrastructure
    PR: #12
    Touch: cksource/cs#13248

---
+++ .circleci/infrastructure/aws/iam.tf
@@ -0,0 +1,47 @@
+locals {
+  project = "cksource-monitoring"
+}
+
+resource "aws_iam_role" "oidc" {
+  name_prefix = "ci-${local.project}-oidc-"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AssumeRole",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : data.terraform_remote_state.common.outputs.oidc.provider_arn
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity"
+        "Condition" : {
+          "StringLike" : {
+            "${data.terraform_remote_state.common.outputs.oidc.provider}/org/${data.terraform_remote_state.common.outputs.oidc.organization_id}:sub" : "org/${data.terraform_remote_state.common.outputs.oidc.organization_id}/project/ec337855-c20c-4c4d-9aaf-dcb87cb7cb94/user/*"
+          }
+        }
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::711372962861:policy/ITECRPolicy",
+  ]
+
+  inline_policy {}
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "terraform_remote_state" "common" {
+  backend = "s3"
+
+  config = {
+    bucket  = "cksource-ci-terraform-states"
+    key     = "cksource/prod/circleci/common"
+    region  = "eu-central-1"
+    profile = "dev"
+  }
+}
---
+++ .circleci/infrastructure/circleci/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = ">= 1.7.4, < 2"
+
+  required_providers {
+    circleci = {
+      source = "mrolla/circleci"
+    }
+  }
+
+  backend "s3" {
+    bucket = "cksource-ci-terraform-states"
+    key    = "cksource/cksource-monitoring/circleci"
+    region = "eu-central-1"
+  }
+}
+
+provider "circleci" {
+  api_token    = var.api_token
+  vcs_type     = "github"
+  organization = local.organization
+}
+
+locals {
+  organization = "cksource"
+  project      = "cksource-monitoring"
+
+  env_variables = {
+    AWS_OIDC_ROLE_ARN = data.terraform_remote_state.aws.outputs.oidc_role_arn
+  }
+}
+
+resource "circleci_environment_variable" "env_variable" {
+  for_each = local.env_variables
+
+  name    = each.key
+  value   = each.value
+  project = local.project
+}
+
+data "terraform_remote_state" "aws" {
+  backend = "s3"
+
+  config = {
+    bucket = "cksource-ci-terraform-states"
+    key    = "cksource/${local.project}/aws"
+    region = "eu-central-1"
+  }
+}
